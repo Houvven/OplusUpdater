99fa77381de9ed26a7a61371fa04c8fb28314f48
Houvven <houvven@gmail.com>
2025-02-10 22:00:26 +0800

    fix test err

--- test/updater_test.go
+++ test/updater_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestQueryUpdater(t *testing.T) {
-	responseCipher, err := updater.QueryUpdater(updater.Attribute{
+	responseCipher, err := updater.QueryUpdater(&updater.Attribute{
 		OtaVer: "RMX3800_11.C",
 	})
 	if err != nil {
