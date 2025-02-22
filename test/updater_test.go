ff41756c1c02b595f36c5df87ae586d02d1f08f3
houvven <houvven@gmail.com>
2025-02-22 10:27:12 +0800

    test(updater): add model-specific update query tests
    - Add separate test functions for different models and regions
    - Implement error handling and pretty printing for test results

--- test/updater_test.go
+++ test/updater_test.go
@@ -3,20 +3,41 @@ package test
 import (
 	"fmt"
 	"github.com/Houvven/OplusUpdater/pkg/updater"
-	"github.com/tidwall/pretty"
 	"testing"
 )
 
-func TestQueryUpdater(t *testing.T) {
-	result := updater.QueryUpdate(&updater.QueryUpdateArgs{
+func TestQueryUpdate_CPH2653_EU(t *testing.T) {
+	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
 		OtaVersion: "CPH2653_11.A",
 		Region:     updater.RegionEu,
 		Model:      "CPH2653EEA",
 	})
-
-	fmt.Printf("Status: %d\n", result.ResponseCode)
-	if result.ResponseCode != 200 {
-		fmt.Printf("Error: %s\n", result.ErrMsg)
+	if err != nil {
+		fmt.Println(err)
 	}
-	fmt.Println(string(pretty.Color(pretty.Pretty(result.DecryptedBodyBytes), nil)))
+	result.PrettyPrint()
+}
+
+func TestQueryUpdate_CPH2653_EU_TEST(t *testing.T) {
+	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
+		OtaVersion: "CPH2653_11.A",
+		Region:     updater.RegionEu,
+		Model:      "CPH2653EEA",
+		Mode:       1,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	result.PrettyPrint()
+}
+
+func TestQueryUpdate_RMX5010_CN(t *testing.T) {
+	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
+		OtaVersion: "RMX5010_11.A",
+		Region:     updater.RegionCn,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	result.PrettyPrint()
 }
