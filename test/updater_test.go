eb91f232ce50fb45660496181bf73f9cfff8014a
Houvven <houvven@gmail.com>
2025-04-07 17:27:04 +0800

    refactor(tests): simplify error handling in query update tests

--- test/updater_test.go
+++ test/updater_test.go
@@ -6,85 +6,65 @@ import (
 	"testing"
 )
 
-func TestQueryUpdate_CPH2653_EU(t *testing.T) {
-	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
-		OtaVersion: "CPH2653_11.A",
-		Region:     updater.RegionEu,
-		Model:      "CPH2653EEA",
-	})
+func ProcessResult(result *updater.ResponseResult, err error) {
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	result.PrettyPrint()
 }
 
+func TestQueryUpdate_CPH2653_EU(t *testing.T) {
+	ProcessResult(updater.QueryUpdate(&updater.QueryUpdateArgs{
+		OtaVersion: "CPH2653_11.A",
+		Region:     updater.RegionEu,
+		Model:      "CPH2653EEA",
+	}))
+}
+
 func TestQueryUpdate_CPH2653_EU_TEST(t *testing.T) {
-	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
+	ProcessResult(updater.QueryUpdate(&updater.QueryUpdateArgs{
 		OtaVersion: "CPH2653_11.A",
 		Region:     updater.RegionEu,
 		Model:      "CPH2653EEA",
 		Mode:       1,
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-	result.PrettyPrint()
+	}))
 }
 
 func TestQueryUpdate_RMX5010_CN(t *testing.T) {
-	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
+	ProcessResult(updater.QueryUpdate(&updater.QueryUpdateArgs{
 		OtaVersion: "RMX5010_11.A",
 		Region:     updater.RegionCn,
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-	result.PrettyPrint()
+	}))
 }
 
 func TestQueryUpdate_RMX5011_RU(t *testing.T) {
-	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
+	ProcessResult(updater.QueryUpdate(&updater.QueryUpdateArgs{
 		OtaVersion: "RMX5011_11.A",
 		Region:     updater.RegionRu,
 		Model:      "RMX5011RU",
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-	result.PrettyPrint()
+	}))
 }
 
 func TestQueryUpdate_RMX5011_TR(t *testing.T) {
-	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
+	ProcessResult(updater.QueryUpdate(&updater.QueryUpdateArgs{
 		OtaVersion: "RMX5011_11.A",
 		Region:     updater.RegionTr,
 		Model:      "RMX5011TR",
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-	result.PrettyPrint()
+	}))
 }
 
 func TestQueryUpdate_RMX5011_TH(t *testing.T) {
-	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
+	ProcessResult(updater.QueryUpdate(&updater.QueryUpdateArgs{
 		OtaVersion: "RMX5011_11.A",
 		Region:     updater.RegionTh,
 		Model:      "RMX5011",
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-	result.PrettyPrint()
+	}))
 }
 
 func TestQueryUpdate_PHP110_CN(t *testing.T) {
-	result, err := updater.QueryUpdate(&updater.QueryUpdateArgs{
+	ProcessResult(updater.QueryUpdate(&updater.QueryUpdateArgs{
 		OtaVersion: "PHP110_11.F",
 		Region:     updater.RegionCn,
-	})
-	if err != nil {
-		fmt.Println(err)
-	}
-	result.PrettyPrint()
+	}))
 }
