d2e5e8c3d088eca389e8140ac2600471c47fc6d9
Houvven <houvven@gmail.com>
2025-05-17 20:27:51 +0800

    feat(updater): enhance command usage and examples in help text

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -22,12 +22,18 @@ func getIntFlag(cmd *cobra.Command, flagName string) int {
 	return flag
 }
 
+var example = `
+  updater CPH2401_11.C.58_0580_202402190800 --model=CPH2401 --region=CN
+  updater RMX3820_13.1.0.130_0130_202404010000 --model=RMX3820 --region=IN --mode=1
+  updater A127_13.0_0001 --model=A127 --carrier=00000000 --proxy=http://localhost:7890
+`
+
 var rootCmd = &cobra.Command{
-	Use:   "updater",
-	Short: " Use Oplus official api to query OPlus,OPPO and Realme Mobile 's OS version update.",
-	Args:  cobra.ExactArgs(1),
+	Use:     "updater [OTA_VERSION]",
+	Short:   "Query OPlus, OPPO and Realme Mobile OS version updates using official API",
+	Example: example,
+	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		//Get the value of the flag
 		otaVer := args[0]
 
 		model := getStringFlag(cmd, "model")
@@ -47,19 +53,19 @@ var rootCmd = &cobra.Command{
 			Proxy:      proxy,
 		})
 		if err != nil {
-			log.Printf("Error in QueryUpdate: %v\n", err)
+			log.Fatalf("Error in QueryUpdate: %v", err)
 		}
 		result.PrettyPrint()
 	},
 }
 
 func init() {
-	rootCmd.Flags().String("region", "CN", "Server zone: CN (default), EU or IN (optional), e.g., --region=CN")
-	rootCmd.Flags().String("model", "", "Device model, e.g., --model=RMX3820")
-	rootCmd.Flags().String("carrier", "", "Found in `my_manifest/build.prop` file, under the `NV_ID` reference, e.g., --carrier=01000100")
-	rootCmd.Flags().Int("mode", 0, "Mode: 0 (stable, default) or 1 (testing), e.g., --mode=0")
-	rootCmd.Flags().String("imei", "", "IMEI, e.g., --imei=86429XXXXXXXX98")
-	rootCmd.Flags().StringP("proxy", "p", "", "Proxy server, e.g., --proxy=type://@host:port or --proxy=type://user:password@host:port, support http and socks proxy")
+	rootCmd.Flags().StringP("model", "m", "", "Device model (required), e.g., RMX3820, CPH2401")
+	rootCmd.Flags().StringP("region", "r", "CN", "Server region: CN (default), EU or IN")
+	rootCmd.Flags().StringP("carrier", "c", "", "Carrier ID found in `my_manifest/build.prop` file under the `NV_ID` reference, e.g., 01000100")
+	rootCmd.Flags().Int("mode", 0, "Mode: 0 (stable, default) or 1 (testing)")
+	rootCmd.Flags().StringP("imei", "i", "", "IMEI, e.g., 864290000000000")
+	rootCmd.Flags().StringP("proxy", "p", "", "Proxy server, e.g., type://user:password@host:port")
 }
 
 func main() {
